a5f482161caa868549dcaef7e5cdfcd557efc336
daffaaliffatur <daffaaliffatur96@gmail.com>
2022-12-13 16:13:19 +0700

    new commits

--- static/script.js
+++ static/script.js
@@ -1,51 +1,11 @@
-const searchForm = document.querySelector('.search-form');
-const searchBtn = document.querySelector('#search-btn');
-const cartItem = document.querySelector('.cart-items-container')
-const cartBtn = document.querySelector('#cart-btn')
-const navbar = document.querySelector('.navbar')
-const menuBtn = document.querySelector('#menu-btn')
-const Containers = document.querySelector('.Containers')
+const navLinks = document.querySelectorAll(".nav-link");
+const navBar = document.querySelector(".navbar-nav");
 
-
-
-searchBtn.addEventListener('click', ()=>{
-    searchForm.classList.toggle('active');
-    document.addEventListener('click', (e)=>{
-        if(!e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm) ){
-            searchForm.classList.remove('active');
-        }
-    })
-})
-
-cartBtn.addEventListener('click', ()=>{
-    cartItem.classList.toggle('active');
-    document.addEventListener('click', (e)=>{
-        if(!e.composedPath().includes(cartBtn) && !e.composedPath().includes(cartItem) ){
-            cartItem.classList.remove('active');
-        }
-    })
-})
-
-menuBtn.addEventListener('click', ()=>{
-    navbar.classList.toggle('active');
-    document.addEventListener('click', (e)=>{
-        if(!e.composedPath().includes(navbar) && !e.composedPath().includes(menuBtn) ){
-            navbar.classList.remove('active');
-        }
-    })
-})
-
-var slidePosition = 0;
-SlideShow();
-
-function SlideShow() {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slidePosition++;
-  if (slidePosition > slides.length) {slidePosition = 1}
-  slides[slidePosition-1].style.display = "block";
-  setTimeout(SlideShow, 2000); // Change image every 2 seconds
-} 
+navBar.addEventListener("click", function (e) {
+  navLinks.forEach((i) => {
+    i.classList.remove("active");
+    if (i.className === e.target.className) {
+      e.target.classList.add("active");
+    }
+  });
+});
